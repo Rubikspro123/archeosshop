be1870ab22f1df614971b2b4b1b26137b9b6fc2e
Rubikspro123 <87181104+Rubikspro123@users.noreply.github.com>
2022-05-07 17:21:11 +0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,134 @@
+value_of_small_hot_coffee = 25;
+value_of_small_cold_coffee = 20;
+value_of_big_hot_coffee = 50;
+value_of_big_cold_coffee = 45;
+answer = 0;
+
+function smalladdhotcoffee(){
+   answer = answer + value_of_small_hot_coffee;
+   document.getElementById("answer").innerHTML = "$" + answer;
+}
+function smalladdcoldcoffee(){
+    answer = answer + value_of_small_cold_coffee;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function bigaddhotcoffee(){
+    answer = answer + value_of_big_hot_coffee;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function bigaddcoldcoffee(){
+    answer = answer + value_of_big_cold_coffee;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+value_of_chips = 40;
+value_of_small_wafer = 35;
+value_of_big_wafer = 55;
+
+function addChips(){
+    answer = answer + value_of_chips;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addsmallwafer(){
+     answer = answer + value_of_small_wafer;
+     document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addbigwafer(){
+     answer = answer + value_of_big_wafer;
+     document.getElementById("answer").innerHTML = "$" + answer;
+}
+
+value_of_base = 50;
+value_of_vanilla_cream = 120;
+value_of_chocolate_cream = 120;
+value_of_grape = 60;
+value_of_strawberry = 50;
+value_of_chocomilk = 46;
+value_of_water = 20;
+
+function addbase(){
+    answer = answer + value_of_base;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addvanillacream(){
+    answer = answer + value_of_vanilla_cream;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addchocolatecream(){
+    answer = answer + value_of_chocolate_cream;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addgrape(){
+    answer = answer + value_of_grape;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addstrawberry(){
+    answer = answer + value_of_strawberry;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addchocomilk(){
+    answer = answer + value_of_chocomilk;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addwater(){
+    answer = answer + value_of_water;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+
+value_of_strawberrypie = 130;
+value_of_darkstrawberrypie = 170;
+value_of_blizzard = 120;
+value_of_cone = 20;
+
+function addstrawberrypie(){
+    answer = answer + value_of_strawberrypie;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function adddarkstrawberrypie(){
+    answer = answer + value_of_darkstrawberrypie;
+    document.getElementById("answer").innerHTML ="$" + answer;
+}
+function addblizzard(){
+    answer = answer + value_of_blizzard;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addcone(){
+    answer = answer + value_of_cone;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+
+value_of_smallfuel = 88;
+value_of_mediumfuel = 126;
+value_of_averagefuel = 168;
+value_of_bigfuel = 240;
+value_of_biggestfuel = 304;
+value_of_bigwater = 90;
+value_of_batteries = 130;
+
+function addsmallfuel(){
+    answer = answer + value_of_smallfuel;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addmediumfuel(){
+    answer = answer + value_of_mediumfuel;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addaveragefuel(){
+    answer = answer + value_of_averagefuel;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addbigfuel(){
+    answer = answer + value_of_bigfuel;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addbiggestfuel(){
+    answer = answer + value_of_biggestfuel;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addbigwater(){
+    answer = answer + value_of_bigwater;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
+function addbatteries(){
+    answer = answer + value_of_batteries;
+    document.getElementById("answer").innerHTML = "$" + answer;
+}
